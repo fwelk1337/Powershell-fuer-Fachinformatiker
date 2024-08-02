4a3c5f3d549d39be71c0b1e52bafe363008eac00
fwelk1337 <fabianwelk@gmail.com>
2024-08-02 12:37:09 +0200

    Updating Modules

--- 02-Module.ps1
+++ 02-Module.ps1
@@ -1,3 +1,6 @@
+# --------------------------------------------------------------------
+# Modules
+# --------------------------------------------------------------------
 # Module sind Erweiturungen deiner Powershell:
 # Wir können diese über die folgenden Befehle installieren und importieren:
 
@@ -12,6 +15,9 @@ Install-Module -Name "AzureAD"
 
 Import-Module -Name "AzureAD"
 
+# --------------------------------------------------------------------
+# Questions
+# --------------------------------------------------------------------
 # Q: Warum müssen wir Module nach dem installieren noch importieren?#
 # A: Deine Powershell arbeitet mit einer Session in der Temporär Daten wie z.B. Variablen und Module geladen werden
 #    dadurch bleibt die Powershell perfomant. 
