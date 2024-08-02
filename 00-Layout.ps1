f6ba2caff22f1807e18de8953c68b2342b756bb0
fwelk1337 <fabianwelk@gmail.com>
2024-08-02 12:14:14 +0200

    init

---
+++ 00-Layout.ps1
@@ -0,0 +1,16 @@
+#region modules
+#endregion
+
+#region variables
+#endregion
+
+#region connects
+#endregion
+
+#region functions
+#endregion
+
+# Start here ->
+#--------------------------------------------------------------------------
+
+# end
